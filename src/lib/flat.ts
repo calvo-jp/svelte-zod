6e1616464258e854a70112b0e577c1c17df5552c
jp calvo <calvojp92@gmail.com>
2024-04-28 15:24:15 +0800

    refactor

--- src/lib/flat.ts
+++ src/lib/flat.ts
@@ -7,7 +7,7 @@ const cache_1 = createLruCache<GenericObject, GenericObject>(100);
 
 export function flatten<T extends GenericObject, R extends GenericObject>(
   target: T,
-  opts?: f.FlattenOptions,
+  options?: f.FlattenOptions,
 ) {
   let cached = cache_0.get(target) as R | undefined;
 
@@ -15,14 +15,14 @@ export function flatten<T extends GenericObject, R extends GenericObject>(
     return cached;
   }
 
-  cached = f.flatten<T, R>(target, opts);
+  cached = f.flatten<T, R>(target, options);
   cache_0.set(target, cached);
   return cached;
 }
 
 export function unflatten<T extends GenericObject, R extends GenericObject>(
   target: T,
-  opts?: f.UnflattenOptions,
+  options?: f.UnflattenOptions,
 ) {
   let cached = cache_1.get(target) as R | undefined;
 
@@ -30,7 +30,7 @@ export function unflatten<T extends GenericObject, R extends GenericObject>(
     return cached;
   }
 
-  cached = f.unflatten<T, R>(target, opts);
+  cached = f.unflatten<T, R>(target, options);
   cache_1.set(target, cached);
   return cached;
 }
